0654c23fd9b5032ea2a1956f9b783936b473b63d
white393939 <2243119644@qq.com>
2026-05-19 18:23:57 +0000

    added intializer and basic functions

---
+++ Block.java
@@ -0,0 +1,62 @@
+public class block {
+    private boolean isMine;
+    private boolean isShown;
+    private int adjMine;
+    
+    public Block()
+    {
+        isMine = false;
+        isShown = false;
+        adjMine = 0;
+    }
+
+    public Block(boolean mine)
+    {
+        isMine = mine;
+        isShown = false;
+        adjMine = 0;
+    }
+
+    public boolean isShown()
+    {
+        return this.isShown;
+    }
+
+    public boolean isMine()
+    {
+        return this.isMine;
+    }
+
+    public void setAdjMine(aMine)
+    {
+        if (isMine)
+        {
+            return;
+        }
+        adjMine = aMine;
+    }
+
+    public void incrimentAdjMine()
+    {
+        if (isMine)
+        {
+            return;
+        }
+        adjMine++;
+    }
+
+    public int getAdjMine()
+    {
+        return adjMine;
+    }
+
+    public void show()
+    {
+        isShown = true;
+    }
+
+    public void setMine()
+    {
+        isMine = true;
+    }
+}
---
+++ Minesweeper.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Minesweeper
+{
+    private block[][] gird;
+    
+    public Minesweeper ()
+    {
+        grid = new block[12][12];
+        initialize(22);
+    }
+
+    public Minesweeper(int nRows, int nCols)
+    {
+        grid = new block[nRows][nCols];
+        initialize((int) (nRows * nCols * 0.15));
+    }
+
+    public Minesweeper(int nRows, int nCols, int nMines)
+    {
+        grid = new block[nRows][nCols];
+        initialize((int) (nMines));
+    }
+
+    public Minesweeper(block[][] g)
+    {
+        grid = g;
+    }
+    
+    private initialize(int nOfMine)
+    {
+        int gRow = grid.length;
+        int gCol = grid[0].length;
+        for (int row = 0; row < gRow; row++)
+        {
+            for (int col = 0; col < gCol; col++)
+            {
+                grid[row][col] = new Block();
+            }
+        }
+        for (int i = 0; i < nOfMine; i++)
+        {
+            do
+            {
+                int row = (int) (Math.random() * gRow);
+                int col = (int) (Math.random() * gCol);
+            }
+            while (grid[row][col].isMine);
+            grid[row][col].setMine();
+            for (int j = row - 1; j < row + 2; j++) //changing numbers of block
+            {
+                for (int k = col - 1; k < col + 2; k++)
+                {
+                    try
+                    {
+                        grid[row][col].incrimentAdjMine();
+                    }
+                    catch(IndexOutOfBoundsException)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
